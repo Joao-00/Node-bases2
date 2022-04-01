d7df990509a44257f00c9f72df861ab5ec1c6be8
Joao Ormeño <joao23.1996@gmail.com>
2022-04-01 12:45:30 -0300

    Node: se instalo paquete de colores y yargs

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
 
 const { crearArchivo } = require('./helpers/multiplicar')
+const argv = require('yargs').argv;
+
 
 console.clear();
 
+console.log(process.argv);
+console.log(argv);
+
+console.log('base: yargs', argv.base);
 
 // con el process.argv podemos ver de donde vienen los argumentos de consola
-const[ , , arg3 = 'base=5' ] = process.argv;
-const[ , base = 5] = arg3.split('=');
+// const[ , , arg3 = 'base=5' ] = process.argv;
+// const[ , base = 5] = arg3.split('=');
 
+// con el comando: npm install colors para colocar colores en la consola de node
 
+//para instalar nodemon solo en desarrollo: npm install nodemon --save-dev
 //const base = 5;
 
+//para instalar versiones especificas de colores: npm i colors@versionespecifica 
+//i = install
+//https://www.npmjs.com/package/colors 
 
 
-crearArchivo(base)
-    .then(nombreArchivo => console.log(nombreArchivo, 'creado'))
-    .catch(err => console.log(err));
+//para instalar yargs: npm i yargs
+//https://www.npmjs.com/package/yargs
+
+
+// crearArchivo(base)
+//     .then(nombreArchivo => console.log(nombreArchivo, 'creado'))
+//     .catch(err => console.log(err));
